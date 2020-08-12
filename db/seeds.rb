bafc7e66fec1a88e2327513d6cc3342c798b87db
Yoann Verdelhan <verdelhan.yoann@gmail.com>
2020-08-12 14:24:07 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+  {
+    name: "La bonne bouffe",
+    address: "21 rue des chardons",
+    phone_number: "0666454443",
+    category: "french"
+  },
+  {
+    name: "La frite",
+    address: "bruxelle, belgium",
+    phone_number: "0777776655",
+    category: "Belgian"
+  },
+  {
+    name: "la pizza",
+    address: "rome, italy",
+    phone_number: "0908090809",
+    category: "italian"
+  },
+  {
+    name: "le sushi",
+    address: "tokyo, japan",
+    phone_number: "0809087654",
+    category: "japanese"
+  },
+  {
+    name: "les pates de poulet",
+    address: "shangai, chine" ,
+    phone_number: "0908765432",
+    category: "chinese"
+  }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
